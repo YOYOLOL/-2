d9463221d7ca71545607a2bc33a74123e4f653bc
YOYOLOL <39958052+YOYOLOL@users.noreply.github.com>
2020-03-04 18:26:01 +0800

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client,msg, args,cmdnumber) => {
+let helpEmbed = new Discord.RichEmbed()
+.setColor('#00E3E3')
+.setTitle('音樂指令')
+.setThumbnail(client.user.avatarURL)
+.setDescription(client.user.username + "是一個小功能Discord機器人。\n如需要查看指令資訊，輸入: .help <指令>")
+.addField("音樂","`play` · `repeat` · `stop` · `skip`")
+.addField('🔠支持語言', '✅中文')
+return msg.channel.send(helpEmbed)
+}
