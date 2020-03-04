6f25e44fb2983d1490970e6b9db7d57a884e74e4
YOYOLOL <39958052+YOYOLOL@users.noreply.github.com>
2020-03-04 18:41:13 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ let helpEmbed = new Discord.RichEmbed()
 .setColor('#00E3E3')
 .setTitle('音樂指令')
 .setThumbnail(client.user.avatarURL)
-.setDescription("我是" + client.user.username + "一個服務大眾的音樂機械人。\n如需要查看指令資訊，輸入: .help <指令>")
+.setDescription("我是" + client.user.username + ",一個服務大眾的音樂機械人。\n如需要查看指令資訊，輸入: .help <指令>")
 .addField(":headphones:音樂","`play` · `repeat` · `stop` · `skip`")
 return msg.channel.send(helpEmbed)
 }
